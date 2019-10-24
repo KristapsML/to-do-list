66c4d0c638f68b018251b809b8a6cad3092d532d
kristapsdesign <kristapsl1803@gmail.com>
2019-10-24 21:39:16 +0200

    Last commit I think

--- src/views/todoList.vue
+++ src/views/todoList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <!-- Header Component -->
     <Header />
     <!-- Wrapper -->
@@ -14,9 +14,8 @@
         <button type="submit" @click.prevent="addTodo()" class="new-todo-button">Add</button>
       </form>
       <ul class="todo-list">
-        <!-- Show todo items, list rendering -->
         <li v-for="todo in todos" :key="todo.id" class="todo-item">
-          <label class="todo-item-label">
+          <label v-if="currentlyEditing !== todo.id" class="todo-item-label">
             <input
               type="checkbox"
               v-model="todo.completed"
@@ -25,6 +24,21 @@
             />
             {{todo.text}}
           </label>
+          <div v-if="currentlyEditing !== todo.id">
+            <button @click="editTodo(todo)" class="todo-button">
+              <img src="../assets/pencil.svg" alt="Edit todo" />
+            </button>
+            <button @click="deleteTodo(todo)" class="todo-button">
+              <img src="../assets/trash.svg" alt="Delete todo" />
+            </button>
+          </div>
+          <form v-else class="edit-todo-form">
+            <label class="edit-todo-label">
+              Edit Exercise:
+              <input type="text" v-model="todoEditText" class="edit-todo-input" />
+            </label>
+            <button type="submit" class="edit-todo-button" @click.prevent="updateTodoText()">Save</button>
+          </form>
         </li>
       </ul>
     </section>
@@ -46,7 +60,9 @@ export default {
   data() {
     return {
       newTodo: "",
-      todos: []
+      todos: [],
+      currentlyEditing: null,
+      todoEditText: ""
     };
   },
   // Firestore returns todos
@@ -58,24 +74,33 @@ export default {
   // Adding todos
   methods: {
     addTodo() {
-      todosCollection
-        .add({
-          text: this.newTodo,
-          completed: false,
-          id: this.todos.length,
-          createdAt: new Date()
-        })
+      // Adding if statement - if NOT an empty string, execute code
+      if (!this.newTodo == "") {
+        todosCollection
+          .add({
+            text: this.newTodo,
+            completed: false,
+            id: this.todos.length,
+            createdAt: new Date()
+          })
 
-        // Console logs when an item is added
-        .then(function(docRef) {
-          console.log("Document written with ID: ", docRef.id);
-        })
-        // Otherwise console logs an error
-        .catch(function(error) {
-          console.error("Error adding document: ", error);
-        });
-      this.newTodo = "";
+          // Console logs when an item is added
+          .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+          })
+          // Otherwise console logs an error
+          .catch(function(error) {
+            console.error("Error adding document: ", error);
+          });
+        this.newTodo = "";
+      }
     },
+    // Deletes an item based on todo.id which is given when an item is added.
+    deleteTodo(todo) {
+      todosCollection.doc(todo.id).delete();
+    },
+
+    // Updated Todo when checkmark is clicked, console logs id, otherwise throws an error.
     updateTodo(todo) {
       todosCollection
         .doc(todo.id)
@@ -86,6 +111,27 @@ export default {
         .catch(function(error) {
           console.error("Error updating document: ", error);
         });
+    },
+    // Takes Item by ID and sets value as the value of currentlyEditing
+    editTodo(todo) {
+      this.currentlyEditing = todo.id;
+      this.todoEditText = todo.text;
+    },
+    // Passes ID to know which item to edit
+    updateTodoText() {
+      todosCollection
+        .doc(this.currentlyEditing)
+        .update({
+          text: this.todoEditText
+        })
+        .then(function(docRef) {
+          console.log("Updated document text with ID: ", todo.id);
+        })
+        .catch(function(error) {
+          console.error("Error updating document text: ", error);
+        });
+      this.currentlyEditing = null;
+      this.todoEditText = "";
     }
   }
 };
@@ -175,5 +221,47 @@ a {
 .todo-list {
   max-width: 100%;
   margin: 2rem auto;
+}
+
+.todo-button {
+  background: transparent;
+  border: 0;
+  padding: 0.5rem;
+  width: 40px;
+  height: 40px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.edit-todo-form {
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
+  padding: 1rem;
+}
+.edit-todo-label {
+  flex: 1;
+  text-align: left;
+  display: flex;
+  align-items: center;
+}
+.edit-todo-input {
+  padding: 0.5rem;
+  border-radius: 3px;
+  border: 1px solid lightgrey;
+  font-size: 1rem;
+  font-weight: normal;
+  flex: 1;
+  margin-left: 1rem;
+}
+.edit-todo-button {
+  font-size: 1rem;
+  padding: 0.5rem 0.7rem;
+  border-radius: 3px;
+  color: #ff4d4d;
+  background: none;
+  font-weight: bold;
+  margin-left: 1rem;
+  border: 1px solid #ff4d4d;
 }
 </style>
